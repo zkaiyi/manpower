852507ae798f997eaf2bbcbd8eff1eca12f964be
zkaiyi <1149189347@qq.com>
2018-12-26 17:39:11 +0800

    個人中心-修改

--- js/font.js
+++ js/font.js
@@ -16,3 +16,19 @@
     //注册翻转事件
     win.addEventListener(resizeEvt, recalc, false);
 })(document, window);
+
+
+
+function timestampToTime(times) {
+    var date = new Date(times );
+    Y = date.getFullYear()+'-';
+    M = (date.getMonth()+1 <10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+    D = (date.getDate() <10 ? '0'+(date.getDate()) : date.getDate()) ;
+    hh = (date.getHours()<10 ? '0'+(date.getHours()) : date.getHours())+':';
+    mm = (date.getMinutes()<10 ? '0'+(date.getMinutes()) : date.getMinutes())+':';
+    ss = (date.getSeconds()<10 ? '0'+(date.getSeconds()) : date.getSeconds());
+    return Y + M + D + hh + mm + ss;
+}
+
+
+
